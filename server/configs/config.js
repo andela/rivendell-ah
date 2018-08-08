530beca017ab2c10fc5f2e0cf1f98bb578a19e69
Chidiebere Ogujeiofor <38565349+chidioguejiofor@users.noreply.github.com>
2018-08-08 17:15:36 +0100

    ft(socialOAuth): user signup via social platform (#29)
    - updates User model
    - adds GOOGLE signup config to .env
    - adds FACEBOOK signup config to .env
    - adds LINKEDIN signup config to .env
    - completes Oauth via Google+
    - creates a dummy heroku app
    - adds Procfile used to set up test heroku app
    - adds an OauthController to the app to handle Passport callbacks
    - adds method to handleRedirect in OauthController
    - refactors payload form
    - adds new tests to the app
    - updates app.json for heroku config
    
    [Finishes #159204696]

--- server/configs/config.js
+++ server/configs/config.js
@@ -9,6 +9,10 @@ module.exports = {
     use_env_variable: 'DATABASE_URL_TEST',
     dialect: 'postgres',
   },
+  staging: {
+    use_env_variable: 'DATABASE_URL_STAGING',
+    dialect: 'postgres',
+  },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
