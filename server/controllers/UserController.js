b183896558dd0e08c4b245ce271549760b97ebde
Mapopo (Evans) <mppdgreat@gmail.com>
2018-09-05 21:09:21 +0100

    let it work

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -194,8 +194,8 @@ class UserController {
       }
       const token = tokenService
         .generateToken({ id: user.id, username: user.username }, 1800);
-      let url = `${process.env.BASEURL}`;
-      url += `/api/users/reset-password?token=${token}`;
+      let url = 'http://127.0.0.1:5500';
+      url += `/reset-password?token=${token}`;
       const mailOptions = emailService.mailOptions(
         user.email,
         "Authors Heaven's account token",
